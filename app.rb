0d578c85ab80990285926a6515274e5f1388fee7
thebluber <thebluber@googlemail.com>
2012-09-08 17:20:28 +0200

    first commit

---
+++ app.rb
@@ -0,0 +1,91 @@
+#encoding:utf-8
+require "sinatra"
+enable :sessions
+
+get '/' do
+    redirect to "/form"
+end
+
+get '/form' do
+    erb :form
+end
+
+get '/favicon.ico' do
+    halt 404
+end
+
+post '/validate' do
+    session['form'] = params.to_a
+    redirect to "/validate"    
+end
+
+get '/validate' do
+    @formData = {}
+    session['form'].each do |el|
+      @formData[el[0]] = el[1]
+    end
+    erb :validate
+end
+
+post '/submit' do
+    data = {}
+    session['form'].each do |el|
+      data[el[0]] = el[1]
+    end
+    person = data["Last Name"] + data["First Name"]
+    time = Time.now.strftime("%d_%m_%Y")
+    file = open("#{person + time}PersonalDataSheet.fdf", "w")
+    file2 = open("#{person}PersonalDataSheet.csv", "w") 
+    file.puts "%FDF-1.2
+    1 0 obj
+    <<
+    /FDF
+    <<
+    /Fields [
+    << /V (2012)/T (Text6) >>"
+    data.each do |k, v|
+      hash = {"ä" => "ae", "Ä" => "Ae", "ü" => "ue", "Ü" => "Ue", "ö" => "oe", "Ö" => "Oe", "ß" => "ss"}
+      old_v = v
+      match_date = v.match(/([0-9]+)-([0-9]+)-([0-9]+)/)
+
+      if (match_date)
+        v = v.gsub("-", "/")
+      end
+
+      hash.each{|key, val| v.gsub!(key, val)}
+      
+      if (k == "DateOfArrival")
+        file.puts "<< /V (2012/04/05)/T (DateOfArrival) >>"
+      elsif (v != "Ja" || v != "Nein")
+        file.puts "<< /V (#{v})/T (#{k}) >>"
+      else
+        file.puts "<< /V /#{v}/T (#{k}) >>"
+      end
+
+
+      file2.puts "#{k},#{old_v}"
+    end
+    
+    file.puts "]
+    >>
+    >>
+    endobj
+    trailer
+    
+    <<
+    /Root 1 0 R
+    >>
+    %%EOF
+    "
+    file.close
+    %x[pdftk PersonalDataSheetRaw2.pdf fill_form #{person + time}PersonalDataSheet.fdf output ./public/#{person + time}output.pdf]
+    session['file'] = "#{person + time}output.pdf"
+    redirect to '/download'
+end
+
+get '/download' do
+    @output = session['file']
+    @zip = "Kyotounterlagen.zip"
+    erb :download
+    #attachment(session['file']) 
+end
---
+++ public/script.js
@@ -0,0 +1,94 @@
+var previousVisit = function (bool){
+  var div = document.getElementById('PreviousVisit');
+  if (bool) {
+    div.innerHTML = "<input class='input-mini' style='display: inline' type='number' name='Times'/> time Latest visit from <input style='dsplay:inline' type='date' name='LastVisitBegin'/> to <input type='date' name='LastVisitEnd'/>"
+  } else {
+    div.innerHTML = "";
+  }
+}
+
+var criminalR = function (bool){
+  var div = document.getElementById('CriminalR');
+  if (bool){
+    div.innerHTML = "<label>Details:</label><input class='input-large' name='CriminalRecord' type='text'/>"
+  } else {
+    div.innerHTML = "";
+  }
+}
+
+var validate = function (){
+  var empty = false;
+  var controlGroups = document.getElementsByClassName("control-group");
+  for (var i = 0; i < controlGroups.length; i++) {
+    var inputs = controlGroups[i].getElementsByTagName("input");
+    for (var j = 0; j < inputs.length; j++){
+      if ((inputs[j].value == null || inputs[j].value == "") && controlGroups[i].className != "control-group error"){
+        controlGroups[i].className += " error";
+        empty = empty || true;
+      }
+    }
+  }
+  if (empty) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+var backToEdit = function() {
+  window.history.back();
+}
+
+var addRel = function(n) {
+  var h = document.createElement("H5");
+  h.innerHTML = "Relative" + n;
+  var div = document.createElement("DIV");
+  div.id = "relDiv" + n;
+  div.className = "control-group";
+  div.appendChild(h);
+  var button = document.createElement("INPUT");
+  button.type = "button";
+  button.value = "+";
+  var arr = ["Name", "Date of birth", "Nationality", "Place of employment", "Status of residence", "Relation to applicant"];
+  var arr2 = ["name", "birth", "national", "empl", "stat", "relation"];
+  for (var i = 0; i < arr.length; i++){
+    var input = document.createElement("INPUT");
+    var label = document.createElement("LABEL");
+      label.innerHTML = arr[i];
+      div.appendChild(label);
+    if (arr[i] != "Date of birth"){
+      input.type = "text";
+    } else {
+      input.type = "date";
+    }
+      input.name = "rel" + n + "_" + arr2[i];  
+      div.appendChild(input);
+  }
+  button.id = "addRel" + n;
+  button.onclick = function (){
+    var div = document.getElementById("relDiv" + n); 
+    if (n < 3) {
+      addRel(n + 1);
+      div.removeChild(document.getElementById("addRel" + n));
+    }
+  };
+  div.appendChild(button);
+  document.getElementsByTagName("fieldset")[0].appendChild(div);
+  div.style = "display: inline-block;";
+
+}
+
+var addRelatives = function() {
+  var relCheck = document.getElementById("rel");
+  if (rel.checked) {
+    addRel(1)
+  } else {
+    document.getElementsByTagName("fieldset")[0].removeChild(document.getElementById("relDiv1"));
+    if (document.getElementById("relDiv2")){
+      document.getElementsByTagName("fieldset")[0].removeChild(document.getElementById("relDiv2"));
+    }
+    if (document.getElementById("relDiv3")){
+      document.getElementsByTagName("fieldset")[0].removeChild(document.getElementById("relDiv3"));
+    }
+  }
+ 
