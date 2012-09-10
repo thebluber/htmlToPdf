29e32d269b068cfb11ef9918f9700d9ef9085170
thebluber <thebluber@googlemail.com>
2012-09-11 00:30:09 +0200

    add datePicker and making some adjustment

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 var previousVisit = function (bool){
   var div = document.getElementById('PreviousVisit');
   if (bool) {
-    div.innerHTML = "<input class='input-mini' style='display: inline' type='number' name='Times'/> time Latest visit from <input style='dsplay:inline' type='date' name='LastVisitBegin'/> to <input type='date' name='LastVisitEnd'/>"
+    div.innerHTML = "<script type='text/javascript'>new JsDatePick({useMode:2,target:'dateField5',dateFormat:'%Y-%m-%d'});new JsDatePick({useMode:2,target:'dateField6',dateFormat:'%Y-%m-%d'});</script><input class='input-mini' id='dateField5' style='display: inline' type='number' name='Times'/> time Latest visit from <input style='dsplay:inline' type='text' id='dateField6' name='LastVisitBegin'/> to <input type='text' name='LastVisitEnd'/>"
   } else {
     div.innerHTML = "";
   }
@@ -13,6 +13,15 @@ var criminalR = function (bool){
     div.innerHTML = "<label>Details:</label><input class='input-large' name='CriminalRecord' type='text'/>"
   } else {
     div.innerHTML = "";
+  }
+}
+
+var checkDate = function (dateFieldId){
+  var dateField = document.getElementById(dateFieldId);
+  var pattern = /[0-9]{4}-[0-9]{2}-[0-9]{2}/g;
+  if (!pattern.test(dateField.value) && dateField.value != ""){
+    dateField.select();
+    alert("Input format: yyyy-mm-dd!");
   }
 }
 
@@ -45,6 +54,16 @@ var backToEdit = function() {
   window.history.back();
 }
 
+var addDatePicker = function(fieldId) {
+    if(!document.getElementById("datePicker" + fieldId)){
+      var script = document.createElement("SCRIPT");
+      script.id = "datePicker" + fieldId;
+      script.type = "text/javascript";
+      script.innerHTML = "new JsDatePick({ useMode:2,target:" + fieldId + ", dateFormat:'%Y-%m-%d'})";
+      document.head.appendChild(script);
+    }
+}
+
 var addRel = function(n) {
   var h = document.createElement("H5");
   h.innerHTML = "Relative" + n;
@@ -62,13 +81,14 @@ var addRel = function(n) {
     var label = document.createElement("LABEL");
       label.innerHTML = arr[i];
       div.appendChild(label);
-    if (arr[i] != "Date of birth"){
+      div.appendChild(input);
       input.type = "text";
-    } else {
-      input.type = "date";
+    if (arr[i] == "Date of birth"){
+      var field = "dateField" + (4 + n);  
+      input.id = field;
+      input.value = "yyyy-mm-dd";
     }
       input.name = "rel" + n + "_" + arr2[i];  
-      div.appendChild(input);
   }
   button.id = "addRel" + n;
   button.onclick = function (){
