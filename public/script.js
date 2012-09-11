235cfe7cb76115f58d38b8ded70a56ce3958cdbd
thebluber <thebluber@googlemail.com>
2012-09-11 15:38:15 +0200

    fix bugs concerning extra dateFields, add datePicker to these fields

--- public/script.js
+++ public/script.js
@@ -1,10 +1,22 @@
 var previousVisit = function (bool){
   var div = document.getElementById('PreviousVisit');
   if (bool) {
-    div.innerHTML = "<script type='text/javascript'>new JsDatePick({useMode:2,target:'dateField5',dateFormat:'%Y-%m-%d'});new JsDatePick({useMode:2,target:'dateField6',dateFormat:'%Y-%m-%d'});</script><input class='input-mini' id='dateField5' style='display: inline' type='number' name='Times'/> time Latest visit from <input style='dsplay:inline' type='text' id='dateField6' name='LastVisitBegin'/> to <input type='text' name='LastVisitEnd'/>"
+    div.innerHTML = "<input class='input-mini' style='display: inline' type='number' name='Times'/> time Latest visit from <input style='dsplay:inline' type='text' id='dateField5' name='LastVisitBegin'/> to <input type='text' id='dateField6' name='LastVisitEnd'/>"
+    addDatePicker('dateField5');
+    addDatePicker('dateField6');
+    document.getElementById('dateField5').onchange = function(){checkDate('dateField5')};
+    document.getElementById('dateField6').onchange = function(){checkDate('dateField6')};
   } else {
     div.innerHTML = "";
   }
+}
+
+var addDatePicker = function (id) {
+		  new JsDatePick({
+			  useMode:2,
+			  target:id,
+			  dateFormat:"%Y-%m-%d"
+		  });
 }
 
 var criminalR = function (bool){
@@ -54,15 +66,6 @@ var backToEdit = function() {
   window.history.back();
 }
 
-var addDatePicker = function(fieldId) {
-    if(!document.getElementById("datePicker" + fieldId)){
-      var script = document.createElement("SCRIPT");
-      script.id = "datePicker" + fieldId;
-      script.type = "text/javascript";
-      script.innerHTML = "new JsDatePick({ useMode:2,target:" + fieldId + ", dateFormat:'%Y-%m-%d'})";
-      document.head.appendChild(script);
-    }
-}
 
 var addRel = function(n) {
   var h = document.createElement("H5");
@@ -81,14 +84,13 @@ var addRel = function(n) {
     var label = document.createElement("LABEL");
       label.innerHTML = arr[i];
       div.appendChild(label);
-      div.appendChild(input);
       input.type = "text";
     if (arr[i] == "Date of birth"){
-      var field = "dateField" + (4 + n);  
+      var field = "dateField" + (6 + n);  
       input.id = field;
-      input.value = "yyyy-mm-dd";
     }
       input.name = "rel" + n + "_" + arr2[i];  
+      div.appendChild(input);
   }
   button.id = "addRel" + n;
   button.onclick = function (){
@@ -101,12 +103,17 @@ var addRel = function(n) {
   div.appendChild(button);
   document.getElementsByTagName("fieldset")[0].appendChild(div);
   div.style = "display: inline-block;";
-
+  var date = document.getElementById("dateField" + (6 + n));
+  if (date){
+    var field = "dateField" + (6 + n);
+    addDatePicker(field);
+    date.onchange = function(){checkDate(field)};
+  }
 }
 
 var addRelatives = function() {
   var relCheck = document.getElementById("rel");
-  if (rel.checked) {
+  if (relCheck.checked) {
     addRel(1)
   } else {
     document.getElementsByTagName("fieldset")[0].removeChild(document.getElementById("relDiv1"));
